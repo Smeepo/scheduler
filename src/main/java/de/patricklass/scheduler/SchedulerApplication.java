506e44d01d08228ea20beb395de1eec88580c6e7
Jens Hieke <jhieke@dxc.com>
2019-08-13 07:34:06 +0200

    AdminMain Push

--- src/main/java/de/patricklass/scheduler/SchedulerApplication.java
+++ src/main/java/de/patricklass/scheduler/SchedulerApplication.java
@@ -1,13 +1,35 @@
 package de.patricklass.scheduler;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class SchedulerApplication {
+public class SchedulerApplication extends Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SchedulerApplication.class, args);
+        launch(args);
     }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        ConfigurableApplicationContext springContext = SpringApplication.run(SchedulerApplication.class);
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("/fxml/adminMain.fxml"));
+        fxmlLoader.setControllerFactory(springContext::getBean);
+
+        Parent rootNode = fxmlLoader.load();
+        primaryStage.setScene(new Scene(rootNode));
+        primaryStage.show();
+    }
+
+
 }
+
+
