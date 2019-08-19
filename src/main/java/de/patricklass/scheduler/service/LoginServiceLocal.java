40acceb821925c03c1b063ff1a3fc563665a575e
Patrick Laß <plass2@dxc.com>
2019-08-19 15:03:53 +0200

    loginservice

--- src/main/java/de/patricklass/scheduler/service/LoginServiceLocal.java
+++ src/main/java/de/patricklass/scheduler/service/LoginServiceLocal.java
@@ -1,6 +1,11 @@
 package de.patricklass.scheduler.service;
 
 import de.patricklass.scheduler.model.User;
+import de.patricklass.scheduler.model.UserCredentials;
+import de.patricklass.scheduler.repository.UserCredentialsRepository;
+import de.patricklass.scheduler.repository.UserRepository;
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.security.auth.login.CredentialException;
@@ -10,9 +15,24 @@ public class LoginServiceLocal implements LoginService {
 
     private User authenticatedUser;
 
+    private final UserCredentialsRepository userCredentialsRepository;
+    private UserRepository userRepository;
+
+    public LoginServiceLocal(UserCredentialsRepository userCredentialsRepository, UserRepository userRepository) {
+        this.userCredentialsRepository = userCredentialsRepository;
+        this.userRepository = userRepository;
+    }
+
     @Override
     public User login(String username, String password) throws CredentialException {
-        return null;
+        UserCredentials credentials = userCredentialsRepository.findByUserName(username);
+        if (BCrypt.checkpw(password, credentials.getPasswordEncrypted())){
+            User user = userRepository.findByUserName(username);
+            this.authenticatedUser = user;
+            return user;
+        } else {
+            throw new CredentialException("Username or Password incorrect");
+        }
     }
 
     @Override
