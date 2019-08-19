1bb29f8dcb164d879f63a3fc350dc47fa1621165
Minh To <minh@to.com>
2019-08-19 08:05:49 +0200

    add field IDs and sample default values

--- src/main/java/de/patricklass/scheduler/PopupAdminEventController.java
+++ src/main/java/de/patricklass/scheduler/PopupAdminEventController.java
@@ -2,16 +2,47 @@ package de.patricklass.scheduler;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import org.springframework.stereotype.Controller;
+
+import java.time.LocalDate;
 
 @Controller
 public class PopupAdminEventController {
+
+
+    @FXML
+    private TextField groupField;
 
     @FXML
     private Button btnCancel;
 
     @FXML
+    private TextArea descriptionArea;
+
+    @FXML
+    private TextField timeField;
+
+    @FXML
+    private DatePicker dateField;
+
+    @FXML
+    private TextField nameField;
+
+    @FXML
     private Button btnCreate;
+
+    @FXML
+    public void initialize(){
+        System.out.println("sdfdsfs");
+        nameField.setText("Lorem Ipsum");
+        timeField.setText("Time as String?");
+        dateField.setValue(LocalDate.now());
+        descriptionArea.setText("Sample description");
+        groupField.setText("Group name");
+    }
 
 
     public void createEvent(){
