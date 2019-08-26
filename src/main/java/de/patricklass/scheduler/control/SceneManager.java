84f4482260bc62784266ca28de50260dbf122ddf
Patrick Laß <plass2@dxc.com>
2019-08-26 07:45:44 +0200

    jdocs for SceneManager.java

--- src/main/java/de/patricklass/scheduler/control/SceneManager.java
+++ src/main/java/de/patricklass/scheduler/control/SceneManager.java
@@ -22,12 +22,24 @@ public class SceneManager {
         this.primaryStage = primaryStage;
     }
 
-    public void addScene(String identifier, Scene scene) {
+    /**
+     * Adds a new entry consisting of the supplied identifier and Scene Object to the {@code sceneMap}
+     * @param identifier the identifier for the new scene
+     * @param scene the new scene
+     * @throws IllegalArgumentException when one of the parameters is null
+     */
+    public void addScene(String identifier, Scene scene) throws IllegalArgumentException{
         if(identifier == null) throw new IllegalArgumentException("Identifier for Scene can't be null");
         if(scene == null) throw new IllegalArgumentException("Scene can't be null");
         sceneMap.put(identifier, scene);
     }
 
+    /**
+     * Selects the scene with the supplied key {@code identifier} form the sceneMap, if it exists and shows it on the
+     * {@code primaryStage. Also pushes the current Scene onto the {@code lastScenes} Stack
+     * @param identifier the identifier of the scene to be shown
+     * @throws IllegalArgumentException if the supplied identifier is null or the map doesn't contain it
+     */
     public void showScene(String identifier) throws IllegalArgumentException {
         if(identifier == null) throw new IllegalArgumentException("Identifier for Scene can't be null");
 
@@ -44,9 +56,13 @@ public class SceneManager {
         primaryStage.setScene(selectedScene);
     }
 
+    /**
+     * Pops the last Scene off of the {@code lastScenes} Stack and shows it on the primaryStage
+     * @throws IllegalStateException if lastScenes is empty
+     */
     public void showLastScene() throws IllegalStateException {
         if (lastScenes.isEmpty()) throw new IllegalStateException("No last Scene");
-        Scene selectedScene = lastScenes.pop();
-        primaryStage.setScene(selectedScene);
+        Scene lastScene = lastScenes.pop();
+        primaryStage.setScene(lastScene);
     }
 }
